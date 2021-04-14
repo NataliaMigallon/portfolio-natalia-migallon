396bc2bbb395a17bef1d258ae6b09ff298fc05ae
NataliaMigallon <natmigallon@gmail.com>
2021-04-14 21:58:46 +0200

    Fixed position eyes

--- src/js/collapsable-eyes.js
+++ src/js/collapsable-eyes.js
@@ -1,10 +1,6 @@
 "use strict";
 
-// const eyeLeftUp = document.querySelectorAll(".js-left-up");
-// const eyeRightUp = document.querySelectorAll(".js-left-down");
-
 const eyesDown = document.querySelectorAll(".js-down");
-//const eyeRightDown = document.querySelectorAll(".js-left-down");
 
 for (let index = 0; index < eyesDown.length; index++) {
   eyesDown[index].addEventListener("click", handleCollap);
