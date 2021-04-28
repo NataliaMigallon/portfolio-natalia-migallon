96ba8e2e678e47a52e7fdbd71c1b31c87cb9d640
NataliaMigallon <natmigallon@gmail.com>
2021-04-28 16:35:47 +0200

    Change menu styles

--- src/js/hamburger.js
+++ src/js/hamburger.js
@@ -1,21 +1,15 @@
 "use strict";
+
 const menu = document.querySelector(".hamburger");
 
-// method
-function toggleMenu(event) {
+function toggleMenu() {
   this.classList.toggle("is-active");
   document.querySelector(".header__nav").classList.toggle("is_active");
-  //event.preventDefault();
 }
-
-// event
 menu.addEventListener("click", toggleMenu, false);
 
-// function handleMenu() {
-//   if (condition) {
-//     this.classList.toggle("is-active");
-//     document.querySelector(".header__nav").classList.toggle("is_active");
-//   } else {
-//   }
+// function toggleMenu() {
+//   this.classList.toggle("is-active");
+//   document.querySelector(".header__nav").classList.toggle("is_active");
 // }
-// menu.addEventListener("click", handleMenu);
+// menu.addEventListener("click", toggleMenu, false);
