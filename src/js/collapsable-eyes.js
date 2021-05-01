aac25e8c508e9623ffdee40f6e051751898d361f
NataliaMigallon <natmigallon@gmail.com>
2021-05-01 22:57:55 +0200

    Add page-projects background-image

--- src/js/collapsable-eyes.js
+++ src/js/collapsable-eyes.js
@@ -1,18 +1,23 @@
 "use strict";
 
 const eyesDown = document.querySelectorAll(".js-down");
-const mouth = document.querySelectorAll(".js-mouth");
+const mouth = document.querySelector(".js-mouth");
 
 for (let index = 0; index < eyesDown.length; index++) {
   eyesDown[index].addEventListener("click", handleCollap);
-  mouth[index].addEventListener("click", handleCollap);
 }
 
 function handleCollap() {
   const parentEye = this.parentNode;
   parentEye.classList.toggle("js-collapsed");
 }
-
+// for (let index = 0; index < mouth.length; index++) {
+//   mouth[index].addEventListener("click", handleCollap2);
+// }
+// function handleCollap2(event) {
+//   const jaja = event.currentTarget;
+//   jaja.classList.toggle("js-collapsed");
+// }
 // function handleCollap(event) {
 //     const clickedEye = event.currentTarget;
 //     const parentEye = clickedEye.parentNode;
