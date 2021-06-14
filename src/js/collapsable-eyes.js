86cf7fddb554c8bb85e06ee9ab28da6dab9fabc9
NataliaMigallon <natmigallon@gmail.com>
2021-06-14 21:49:42 +0200

    Add background effect in soft skills and fixed SASS errors in parenthesis

--- src/js/collapsable-eyes.js
+++ src/js/collapsable-eyes.js
@@ -1,19 +1,19 @@
 "use strict";
 
-// (function () {
-//   const toggleElements = document.querySelectorAll(".js-toggle");
+(function () {
+  const toggleElements = document.querySelectorAll(".js-toggle");
 
-//   // for (let index = 0; index < toggleElements.length; index++) {
-//   //   toggleElements[index].addEventListener("click", handleCollap);
-//   // }
-//   toggleElements.forEach((element) => {
-//     element.addEventListener("click", handleCollap);
-//   });
-//   function handleCollap() {
-//     const parentCollapsable = this.parentNode;
-//     parentCollapsable.classList.toggle("js-collapsed");
-//   }
-// })(); // espera a que todo DOM cargado para hacerlo funcionar
+  // for (let index = 0; index < toggleElements.length; index++) {
+  //   toggleElements[index].addEventListener('click', handleCollap);
+  // }
+  toggleElements.forEach((element) => {
+    element.addEventListener("click", handleCollap);
+  });
+  function handleCollap() {
+    const parentCollapsable = this.parentNode;
+    parentCollapsable.classList.toggle("js-collapsed");
+  }
+})(); // espera a que todo DOM cargado para hacerlo funcionar
 
 // const showEyeUp = () => {
 //   const eyeUp = document.querySelector("eye__up");
