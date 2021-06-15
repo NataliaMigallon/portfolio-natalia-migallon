2a5c96cd4f938cbb06756134ee4b2479ab0336a3
NataliaMigallon <natmigallon@gmail.com>
2021-06-15 12:26:36 +0200

    Add loader

---
+++ src/js/loader.js
@@ -0,0 +1,7 @@
+"use strict";
+
+window.onload = function () {
+  const container = document.querySelector(".lds-roller");
+  container.style.visibility = "hidden";
+  container.style.opacity = "0";
+};
